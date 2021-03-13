b18bd29eee9b702e53b0b34557776930a949b20f
Adam Choragwicki <adamchor1992@gmail.com>
2021-03-13 01:29:49 +0100

    Add clickability to pieces

--- Checkers/piece.cpp
+++ Checkers/piece.cpp
@@ -1,7 +1,10 @@
 #include "piece.h"
 #include <QPen>
+#include <QGraphicsSceneMouseEvent>
 
-Piece::Piece(std::pair<int, int> coordinates, Player player)
+Piece* Piece::m_PieceCurrentlyChosen = nullptr;
+
+Piece::Piece(std::pair<int, int>& coordinates, Player player) : m_Player(player)
 {
     QGraphicsEllipseItem::setRect((coordinates.second - 1) * Common::TILE_SIZE + PIECE_OFFSET_X,
                                   (coordinates.first - 1) * Common::TILE_SIZE + PIECE_OFFSET_Y,
@@ -11,12 +14,44 @@ Piece::Piece(std::pair<int, int> coordinates, Player player)
     row = coordinates.first;
     column = coordinates.second;
 
-    if(player == Player::Down)
+    if(m_Player == Player::Down)
     {
         setBrush(BLACK_PIECE_COLOR);
         setPen(QPen(QBrush(BLACK_PIECE_OUTLINE_COLOR), PIECE_OUTLINE_WIDTH));
     }
-    else if(player == Player::Up)
+    else if(m_Player == Player::Up)
+    {
+        setBrush(RED_PIECE_COLOR);
+        setPen(QPen(QBrush(RED_PIECE_OUTLINE_COLOR), PIECE_OUTLINE_WIDTH));
+    }
+}
+
+void Piece::mousePressEvent(QGraphicsSceneMouseEvent* event)
+{
+    if(event->button() == Qt::MouseButton::LeftButton)
+    {
+        if(m_PieceCurrentlyChosen != nullptr)
+        {
+            m_PieceCurrentlyChosen->Unclicked();
+        }
+        m_PieceCurrentlyChosen = this;
+        m_PieceCurrentlyChosen->Clicked();
+    }
+}
+
+void Piece::Clicked()
+{
+    setPen(QPen(QBrush(PIECE_CHOSEN_OUTLINE_COLOR), PIECE_OUTLINE_WIDTH));
+}
+
+void Piece::Unclicked()
+{
+    if(m_Player == Player::Down)
+    {
+        setBrush(BLACK_PIECE_COLOR);
+        setPen(QPen(QBrush(BLACK_PIECE_OUTLINE_COLOR), PIECE_OUTLINE_WIDTH));
+    }
+    else if(m_Player == Player::Up)
     {
         setBrush(RED_PIECE_COLOR);
         setPen(QPen(QBrush(RED_PIECE_OUTLINE_COLOR), PIECE_OUTLINE_WIDTH));
--- Checkers/piece.h
+++ Checkers/piece.h
@@ -6,9 +6,11 @@
 class Piece : public QGraphicsEllipseItem
 {
 public:
-    Piece(std::pair<int, int> coordinates, Player player);
+    Piece(std::pair<int, int>& coordinates, Player player);
 
 private:
+    static Piece* m_PieceCurrentlyChosen;
+
     int row;
     int column;
 
@@ -21,4 +23,12 @@ private:
     const QColor BLACK_PIECE_OUTLINE_COLOR{0, 0, 0};
     const QColor RED_PIECE_COLOR{220, 0, 0};
     const QColor RED_PIECE_OUTLINE_COLOR{170, 0, 0};
+    const QColor PIECE_CHOSEN_OUTLINE_COLOR{255, 255, 0};
+
+    Player m_Player;
+
+    void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
+
+    void Clicked();
+    void Unclicked();
 };
