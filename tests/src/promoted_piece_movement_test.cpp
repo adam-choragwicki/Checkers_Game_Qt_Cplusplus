da6480d8eafb4895f603572242affa7fd7ac56e3
Adam Choragwicki <adam.choragwicki.dev@gmail.com>
2025-12-29 09:30:26 +0100

    Refactor promoted_piece_movement_test.cpp

--- tests/src/promoted_piece_movement_test.cpp
+++ tests/src/promoted_piece_movement_test.cpp
@@ -6,14 +6,14 @@ class PromotedPieceMovementTest : public CommonTestFixture
 
 TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesInTheirFirstRows)
 {
-    piecesParameters_ = {{{1, 4}, Player::NORTH, true},
-                         {{1, 6}, Player::NORTH, true},
-                         {{8, 3}, Player::SOUTH, true},
-                         {{8, 5}, Player::SOUTH, true}};
+    setup({
+        {{1, 4}, Player::NORTH, true},
+        {{1, 6}, Player::NORTH, true},
+        {{8, 3}, Player::SOUTH, true},
+        {{8, 5}, Player::SOUTH, true}
+    });
 
-    placePiecesOnCheckerboard();
-
-    for(auto& piece : piecesOnCheckerboard_)
+    for (auto& piece: piecesOnCheckerboard_)
     {
         EXPECT_TRUE(PieceMovementManager::checkIfPieceCanMove(*piece, piecesManager_));
     }
@@ -21,14 +21,14 @@ TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesInTheirFirst
 
 TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesInMiddleRows)
 {
-    piecesParameters_ = {{{3, 4}, Player::NORTH, true},
-                         {{3, 6}, Player::NORTH, true},
-                         {{6, 3}, Player::SOUTH, true},
-                         {{6, 5}, Player::SOUTH, true}};
+    setup({
+        {{3, 4}, Player::NORTH, true},
+        {{3, 6}, Player::NORTH, true},
+        {{6, 3}, Player::SOUTH, true},
+        {{6, 5}, Player::SOUTH, true}
+    });
 
-    placePiecesOnCheckerboard();
-
-    for(auto& piece : piecesOnCheckerboard_)
+    for (auto& piece: piecesOnCheckerboard_)
     {
         EXPECT_TRUE(PieceMovementManager::checkIfPieceCanMove(*piece, piecesManager_));
     }
@@ -36,14 +36,14 @@ TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesInMiddleRows
 
 TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesInSideColumns)
 {
-    piecesParameters_ = {{{3, 8}, Player::NORTH, true},
-                         {{4, 1}, Player::NORTH, true},
-                         {{5, 8}, Player::SOUTH, true},
-                         {{6, 1}, Player::SOUTH, true}};
+    setup({
+        {{3, 8}, Player::NORTH, true},
+        {{4, 1}, Player::NORTH, true},
+        {{5, 8}, Player::SOUTH, true},
+        {{6, 1}, Player::SOUTH, true}
+    });
 
-    placePiecesOnCheckerboard();
-
-    for(auto& piece : piecesOnCheckerboard_)
+    for (auto& piece: piecesOnCheckerboard_)
     {
         EXPECT_TRUE(PieceMovementManager::checkIfPieceCanMove(*piece, piecesManager_));
     }
@@ -51,18 +51,18 @@ TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesInSideColumn
 
 TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesHalfBlockedByOtherFriendlyPieces)
 {
-    piecesParameters_ = {{{3, 2}, Player::NORTH, true},
-                         {{4, 3}, Player::NORTH, true},
-                         {{3, 6}, Player::NORTH, true},
-                         {{4, 5}, Player::NORTH, true},
-                         {{7, 2}, Player::SOUTH, true},
-                         {{6, 3}, Player::SOUTH, true},
-                         {{7, 6}, Player::SOUTH, true},
-                         {{6, 5}, Player::SOUTH, true}};
+    setup({
+        {{3, 2}, Player::NORTH, true},
+        {{4, 3}, Player::NORTH, true},
+        {{3, 6}, Player::NORTH, true},
+        {{4, 5}, Player::NORTH, true},
+        {{7, 2}, Player::SOUTH, true},
+        {{6, 3}, Player::SOUTH, true},
+        {{7, 6}, Player::SOUTH, true},
+        {{6, 5}, Player::SOUTH, true}
+    });
 
-    placePiecesOnCheckerboard();
-
-    for(auto& piece : piecesOnCheckerboard_)
+    for (auto& piece: piecesOnCheckerboard_)
     {
         EXPECT_TRUE(PieceMovementManager::checkIfPieceCanMove(*piece, piecesManager_));
     }
@@ -70,18 +70,18 @@ TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesHalfBlockedB
 
 TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesHalfBlockedByOtherEnemyPieces)
 {
-    piecesParameters_ = {{{3, 2}, Player::NORTH, true},
-                         {{4, 3}, Player::SOUTH, true},
-                         {{3, 6}, Player::NORTH, true},
-                         {{4, 5}, Player::SOUTH, true},
-                         {{7, 2}, Player::SOUTH, true},
-                         {{6, 3}, Player::NORTH, true},
-                         {{7, 6}, Player::SOUTH, true},
-                         {{6, 5}, Player::NORTH, true}};
+    setup({
+        {{3, 2}, Player::NORTH, true},
+        {{4, 3}, Player::SOUTH, true},
+        {{3, 6}, Player::NORTH, true},
+        {{4, 5}, Player::SOUTH, true},
+        {{7, 2}, Player::SOUTH, true},
+        {{6, 3}, Player::NORTH, true},
+        {{7, 6}, Player::SOUTH, true},
+        {{6, 5}, Player::NORTH, true}
+    });
 
-    placePiecesOnCheckerboard();
-
-    for(auto& piece : piecesOnCheckerboard_)
+    for (auto& piece: piecesOnCheckerboard_)
     {
         EXPECT_TRUE(PieceMovementManager::checkIfPieceCanMove(*piece, piecesManager_));
     }
@@ -89,14 +89,14 @@ TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesHalfBlockedB
 
 TEST_F(PromotedPieceMovementTest, CheckIfPiecesCanMovePositive_PiecesInTheirLastRows)
 {
-    piecesParameters_ = {{{8, 3}, Player::NORTH, true},
-                         {{8, 5}, Player::NORTH, true},
-                         {{1, 4}, Player::SOUTH, true},
-                         {{1, 6}, Player::SOUTH, true}};
+    setup({
+        {{8, 3}, Player::NORTH, true},
+        {{8, 5}, Player::NORTH, true},
+        {{1, 4}, Player::SOUTH, true},
+        {{1, 6}, Player::SOUTH, true}
+    });
 
-    placePiecesOnCheckerboard();
-
-    for(auto& piece : piecesOnCheckerboard_)
+    for (auto& piece: piecesOnCheckerboard_)
     {
         EXPECT_TRUE(PieceMovementManager::checkIfPieceCanMove(*piece, piecesManager_));
     }
@@ -104,18 +104,18 @@ TEST_F(PromotedPieceMovementTest, CheckIfPiecesCanMovePositive_PiecesInTheirLast
 
 TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesBlockedInFrontByEnemyPiecesAndWall)
 {
-    piecesParameters_ = {{{4, 1}, Player::NORTH, true},
-                         {{4, 3}, Player::NORTH, true},
-                         {{4, 5}, Player::NORTH, true},
-                         {{4, 7}, Player::NORTH, true},
-                         {{5, 2}, Player::SOUTH, true},
-                         {{5, 4}, Player::SOUTH, true},
-                         {{5, 6}, Player::SOUTH, true},
-                         {{5, 8}, Player::SOUTH, true}};
+    setup({
+        {{4, 1}, Player::NORTH, true},
+        {{4, 3}, Player::NORTH, true},
+        {{4, 5}, Player::NORTH, true},
+        {{4, 7}, Player::NORTH, true},
+        {{5, 2}, Player::SOUTH, true},
+        {{5, 4}, Player::SOUTH, true},
+        {{5, 6}, Player::SOUTH, true},
+        {{5, 8}, Player::SOUTH, true}
+    });
 
-    placePiecesOnCheckerboard();
-
-    for(auto& piece : piecesOnCheckerboard_)
+    for (auto& piece: piecesOnCheckerboard_)
     {
         EXPECT_TRUE(PieceMovementManager::checkIfPieceCanMove(*piece, piecesManager_));
     }
@@ -123,66 +123,66 @@ TEST_F(PromotedPieceMovementTest, CheckIfPieceCanMovePositive_PiecesBlockedInFro
 
 TEST_F(PromotedPieceMovementTest, CheckMovePossibility1)
 {
-    piecesParameters_ = {{{3, 4}, Player::NORTH, true},
-                         {{6, 5}, Player::SOUTH, true}};
+    setup({
+        {{3, 4}, Player::NORTH, true},
+        {{6, 5}, Player::SOUTH, true}
+    });
 
-    placePiecesOnCheckerboard();
+    const std::vector<Coordinates> targetsPlayer1True = {{4, 3}, {4, 5}, {2, 3}, {2, 5}};
+    const std::vector<Coordinates> targetsPlayer1False = {{1, 2}, {1, 6}, {5, 2}, {5, 6}, {3, 2}, {3, 6}, {1, 4}, {5, 4}};
 
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(4, 3)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(4, 5)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(2, 3)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(2, 5)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(1, 2)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(1, 6)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(5, 2)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(5, 6)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(3, 2)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(3, 6)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(1, 4)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(5, 4)));
+    for (auto& target: targetsPlayer1True)
+    {
+        EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, target));
+    }
 
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(5, 4)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(5, 6)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(7, 4)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(7, 6)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(4, 3)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(4, 7)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(8, 3)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(8, 7)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(4, 5)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(8, 5)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(6, 3)));
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(6, 7)));
+    for (auto& target: targetsPlayer1False)
+    {
+        EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, target));
+    }
+
+    const std::vector<Coordinates> targetsPlayer2True = {{5, 4}, {5, 6}, {7, 4}, {7, 6}};
+    const std::vector<Coordinates> targetsPlayer2False = {{4, 3}, {4, 7}, {8, 3}, {8, 7}, {4, 5}, {8, 5}, {6, 3}, {6, 7}};
+
+    for (auto& target: targetsPlayer2True)
+    {
+        EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, target));
+    }
+
+    for (auto& target: targetsPlayer2False)
+    {
+        EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, target));
+    }
 }
 
 TEST_F(PromotedPieceMovementTest, CheckMovePossibility2)
 {
-    piecesParameters_ = {{{4, 5}, Player::NORTH, true},
-                         {{5, 4}, Player::SOUTH, true}};
+    setup({
+        {{4, 5}, Player::NORTH, true},
+        {{5, 4}, Player::SOUTH, true}
+    });
 
-    placePiecesOnCheckerboard();
+    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, {5, 4}));
+    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, {5, 6}));
+    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, {3, 4}));
+    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, {3, 6}));
 
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(5, 4)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(5, 6)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(3, 4)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(0), piecesManager_, Coordinates(3, 6)));
-
-    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(4, 5)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(4, 3)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(6, 3)));
-    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, Coordinates(6, 5)));
+    EXPECT_FALSE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, {4, 5}));
+    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, {4, 3}));
+    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, {6, 3}));
+    EXPECT_TRUE(PieceMovementManager::checkMovePossibility(*piecesOnCheckerboard_.at(1), piecesManager_, {6, 5}));
 }
 
 TEST_F(PromotedPieceMovementTest, WhichPiecesCanMove)
 {
-    piecesParameters_ = {{{7, 2}, Player::NORTH, true},
-                         {{8, 3}, Player::NORTH, true},
-                         {{1, 2}, Player::SOUTH, true},
-                         {{4, 5}, Player::SOUTH, true},
-                         {{3, 4}, Player::SOUTH, true},
-                         {{3, 6}, Player::SOUTH, true}};
-
-    placePiecesOnCheckerboard();
+    setup({
+        {{7, 2}, Player::NORTH, true},
+        {{8, 3}, Player::NORTH, true},
+        {{1, 2}, Player::SOUTH, true},
+        {{4, 5}, Player::SOUTH, true},
+        {{3, 4}, Player::SOUTH, true},
+        {{3, 6}, Player::SOUTH, true}
+    });
 
     std::vector<Piece*> piecesWhichCanMovePlayerUp = PieceMovementManager::whichPiecesCanMove(Player::NORTH, piecesManager_);
     std::vector<Piece*> piecesWhichCanMovePlayerDown = PieceMovementManager::whichPiecesCanMove(Player::SOUTH, piecesManager_);
